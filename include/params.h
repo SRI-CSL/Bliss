70845252048e8e93f85674d50b06922ce52ff308
Ian A. Mason <iam@csl.sri.com>
2016-10-27 16:22:53 -0700

    Comments and the horses mouth.

--- include/params.h
+++ include/params.h
@@ -1,11 +1,20 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
+
+/*
+ * We are working over ZZ[x] / (x^N +1) 
+ * with coefficients modulo Q = 12289.
+ * 
+ * N = 1024
+ * Q = 12289
+ *
+ */
 #define PARAM_N 1024
 
-#define PARAM_K 16
-
 #define PARAM_Q 12289 
+
+#define PARAM_K 16
 
 #define POLY_BYTES 1792
 
