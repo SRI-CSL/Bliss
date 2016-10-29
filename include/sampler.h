e51d06b15d5edf25b2ff981e5d05f88e2ca56c1c
Ian A. Mason <iam@csl.sri.com>
2016-10-29 13:59:34 -0700

    cleaning up the api, now that we have the tables.

--- include/sampler.h
+++ include/sampler.h
@@ -5,43 +5,57 @@
 #include "entropy.h"
 
 
-/* Gaussian "tail-cut" factor */
-const int tau = 12;
-
-/*  sqrt(2 ln 2)     */
-const double sigma_bin_inv_lowprec = 1.17741002251547469101156932645969963;
-
-/*  sqrt(1/(2 ln 2)) */
-const double sigma_bin_lowprec = 0.8493218002880190427215028341028896; 
-
-
 typedef struct sampler_s {
-  entropy_t entropy;
-  int64_t stddev;
-  double alpha;
-
-  uint64_t k;      /* called k in Sampler.[h.cpp]                     */
-
-  uint64_t mask;   /* called mask in Sampler.[h.cpp]                  */
-  
-  uint32_t ell;    /* appears to be the "l" in algorithm 8            */
-  uint8_t* c;      /* appears to be the precomputation in algorithm 8 */
-  
+  entropy_t entropy;   /* BD thinks we should use a function pointer; rather than hard code 
+			  this sepcific implementation. Seems reasonable. TODO.
+		       */
+  uint8_t *c;          /* the table we will use (from tables.h) */
+  uint32_t stddev;
+  uint32_t ell;        /* rows in the table     */
+  uint32_t precision;  /* columns = precision/8 */
 } sampler_t;
 
 
-extern bool sampler_init(sampler_t*, int64_t, double);
+extern bool sampler_init(sampler_t* sampler, uint32_t stddev, uint32_t ell, uint32_t precision);
 
-extern void sampler_delete(sampler_t*);
+extern void sampler_delete(sampler_t* sampler);
 
-/* sampling Bernoulli_p with p a constant in [0, 1] */
-extern bool sampler_ber(sampler_t*, const uint8_t*);
+/* sampling Bernoulli_p with p a constant in [0, 1] TODO add the length of val ? 
 
-/* sampling Bernoulli_E with E = exp(-x/(2*sigma*sigma) */
-extern bool sampler_ber_exp(sampler_t*, uint64_t);
+   p is represented as an array of bytes, val.
 
-/* sampling Bernoulli_C with C = 1/cosh(x/(sigma*sigma) */
-extern bool sampler_ber_cosh(sampler_t*, int64_t);
+   returns true is the sampling was successful, false if something went wrong
+
+   accepted will  point to true in the value was accepted, false if it was rejected.
+*/
+extern bool sampler_ber(sampler_t* sampler, const uint8_t* val, bool* accepted);
+
+/* sampling Bernoulli_E with E = exp(-x/(2*sigma*sigma)
+
+   returns true is the sampling was successful, false if something went wrong
+
+   accepted will  point to true in the value was accepted, false if it was rejected.
+
+ */
+extern bool sampler_ber_exp(sampler_t* sampler, uint64_t val, bool* accepted);
+
+/* sampling Bernoulli_C with C = 1/cosh(x/(sigma*sigma)
+
+   returns true is the sampling was successful, false if something went wrong
+
+   accepted will  point to true in the value was accepted, false if it was rejected.
+
+ */
+extern bool sampler_ber_cosh(sampler_t* sampler, int64_t val, bool* accepted);
+
+/* sampling Bernoulli_C with C = 1/cosh(x/(sigma*sigma)
+
+   returns true is the sampling was successful, false if something went wrong
+
+   accepted will  point to true in the value was accepted, false if it was rejected.
+
+ */
+extern bool sampler_ber_gauss(sampler_t* sampler, int64_t val, bool* accepted);
 
 
 
--- src/sampler.c
+++ src/sampler.c
@@ -2,100 +2,15 @@
 #include <mpfr.h>
 
 #include "sampler.h"
-
-static void sampler_init_k(sampler_t *sampler){
-
-  sampler->k = (uint64_t) (sigma_bin_inv_lowprec * sampler->stddev) + 1;
-
-
-}
-
-static void sampler_init_mask(sampler_t *sampler){
-
-
-}
-
-static void sampler_init_f(sampler_t *sampler, mpfr_t f){
-
-
-}
+#include "tables.h"
 
 
 
-
-static void sampler_init_c(uint32_t ell, uint8_t *c, mpfr_t f){
-  int32_t i, j;
-  mpfr_t y, t, z;
-  mpfr_init2(t, 128);
-  mpfr_init2(z, 128);
-  mpfr_init2(y, 128);
-
-  
-  mpfr_set_ui(t, 8, GMP_RNDN);
-  /* t = 2^8 */
-  mpfr_exp2(t, t, GMP_RNDN);
-
-
-  mpfr_set_ui(y, 1, GMP_RNDN);
- 
-  for(i = 0; i < ell; i++){
-
-    /*  z = 2^i */
-    mpfr_set(z, y, GMP_RNDN); 
-
-    /*  z = -2^i */
-    mpfr_mul_si(z, z, -1, GMP_RNDN);
-
-    /*  z = -2^i/f      */
-    mpfr_div(z, z, f, GMP_RNDN); 
-
-    /*  z = exp(-2^i/f) */
-    mpfr_exp(z, z, GMP_RNDN); 
-
-    for (j=0; j<16; j++) {
-      /*  z = exp(-2^i/f)*2^8  */
-      mpfr_mul(z, z, t, GMP_RNDN); 
-      
-      c[i*16+j] = (uint8_t)mpfr_get_ui(z, GMP_RNDD);
-
-      /* ? */
-      mpfr_sub_ui(z, z, (uint64_t) c[i*16+j], GMP_RNDN);
-
-    }
-
-    /* y = 2 * y */
-    mpfr_mul_ui(y,y, 2, GMP_RNDN); 
-    
-  }
-}
-
-
-
-
-bool sampler_init(sampler_t *sampler, int64_t stddev, double alpha){
+bool sampler_init(sampler_t *sampler, uint32_t stddev, uint32_t ell, uint32_t precision){
   if(sampler != NULL){
-    mpfr_t f;
-    mpfr_init2(f, 128);
-
     sampler->stddev = stddev;
-
-    sampler->alpha = alpha;
-
-    sampler_init_k(sampler);
-
-    sampler_init_mask(sampler);
-
-    sampler_init_f(sampler, f);
-    
-    sampler->ell = 64;  /* need to grok the obscure calculation of this */
-
-    sampler->c = calloc(16 * sampler->ell, sizeof(uint8_t));
-    if(sampler->c !=  NULL){ 
-      sampler_init_c(sampler->ell, sampler->c, f);
-
-      return entropy_init(&sampler->entropy);
-    }
-    mpfr_clear(f);
+    sampler->ell = ell;
+    sampler->precision = precision;
   }
   return false;
 }
@@ -110,30 +25,33 @@ void sampler_delete(sampler_t *sampler){
 
 
 /* sampling Bernoulli_c with c a constant in [0, 1] */
-bool sampler_ber(sampler_t* sampler, const uint8_t* p){
-  if(sampler != NULL && p != NULL){
+bool sampler_ber(sampler_t* sampler, const uint8_t* p, bool* accepted){
+  if(sampler != NULL && p != NULL && accepted != NULL){
     uint8_t i, uc;
     for(i = 0; i < 16; i++){
       uc = entropy_random_uint8(&sampler->entropy);
-      if(uc < *p){ return true; }
-      if(uc > *p){ return false; }
+      if(uc < *p){ *accepted = true; return true; }
+      if(uc > *p){ *accepted = false; return true; }
       p++;  //iam: huh?
     }
   }
-  return true;
+  return false;
 }
 
 /* sampling Bernoulli_E with E = exp(-x/(2*sigma*sigma) */
-bool sampler_ber_exp(sampler_t* sampler, uint64_t x){
+bool sampler_ber_exp(sampler_t* sampler, uint64_t x, bool* accepted){
+  if(sampler != NULL && accepted != NULL){
 
-
-
-  return true;
+    return true;
+  }
+  return false;
 }
 
 /* sampling Bernoulli_C with C = 1/cosh(x/(sigma*sigma) */
-bool sampler_ber_cosh(sampler_t* sampler, int64_t x){
+bool sampler_ber_cosh(sampler_t* sampler, int64_t x, bool* accepted){
+  if(sampler != NULL && accepted != NULL){
 
-
-  return true;
+    return true;
+  }
+  return false;
 }
