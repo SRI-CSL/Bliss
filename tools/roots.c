4cb6cf4a936435acc43a160edbc31a3917d97078
Ian A. Mason <iam@csl.sri.com>
2016-11-02 14:58:48 -0700

    playing with roots.

--- tools/roots.c
+++ tools/roots.c
@@ -1,15 +1,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <gmp.h>
 
+
+static bool is_nth_root_of_unity_mod_q(uint32_t ii, uint32_t ni, uint32_t qi){
+  bool retval;
+  mpz_t q, n, r, i;
+  
+  mpz_init(q);
+  mpz_init(n);
+  mpz_init(i);
+  mpz_init(r);
+
+  mpz_set_ui(q, qi);
+  mpz_set_ui(n, ni);
+
+  mpz_set_ui(i, ii);
+  mpz_powm_sec(r, i, n, q);
+
+  retval = mpz_cmp_ui(r, 1) == 0 ? true : false;
+  
+  mpz_clear(q);
+  mpz_clear(n);
+  mpz_clear(i);
+  mpz_clear(r);
+
+  return retval;
+}
+
+
+static bool is_primitive_nth_root_of_unity_mod_q(uint32_t ii, uint32_t ni, uint32_t qi){
+  uint32_t ri;
+  
+  if(is_nth_root_of_unity_mod_q(ii, ni, qi)){
+    for( ri = 1; ri <  ni; ri++){
+      if(is_nth_root_of_unity_mod_q(ii, ri, qi)){   return false; }
+    }
+
+    return true;
+
+  } 
+
+  return false;
+
+}
 
 
 
 int main(int argc, char* argv[]){
   uint32_t qi, ni, ii;
-  mpz_t q, n, r, i;
+
+  uint32_t ucount, pcount;
   
   if(argc != 3){
     fprintf(stderr, "Usage: %s <q (e.g. 12289)> <n (e.g. 512 1024)>\n", argv[0]);
@@ -20,34 +64,25 @@ int main(int argc, char* argv[]){
 
   ni = atol(argv[2]);
 
-  mpz_init(q);
-  mpz_init(n);
-  mpz_init(i);
-  mpz_init(r);
+  ucount = 0;
 
-  mpz_set_ui(q, qi);
-  mpz_set_ui(n, ni);
-
+  pcount = 0;
   
   for(ii = 1; ii < qi; ii++){
-    mpz_set_ui(i, ii);
-    mpz_powm_sec(r, i, n, q);
 
-    if(!mpz_cmp_ui(r, 1)){
-      fprintf(stderr, "%d is a root of unity\n", ii);
+    if(is_nth_root_of_unity_mod_q(ii, ni, qi)){
+      fprintf(stderr, "%d is a root of unity", ii);
+      ucount++;
+      if(is_primitive_nth_root_of_unity_mod_q(ii, ni, qi)){
+	pcount++;
+	fprintf(stderr, " and is primitive!\n");
+      } else {
+	fprintf(stderr, ".\n");
+      }
     }
-
   }
-  
 
-
-  mpz_clear(q);
-  mpz_clear(n);
-  mpz_clear(i);
-  mpz_clear(r);
-
-
-
+  fprintf(stderr, "%d roots of which %d are primitive\n", ucount, pcount);
 
   return 0;
 }
