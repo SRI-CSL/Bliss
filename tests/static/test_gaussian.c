fb2b4b0f5f7475041390367317894f843164b51b
Bruno Dutertre <bruno@csl.sri.com>
2016-11-14 09:52:02 -0800

    Test of Gaussian and Pos-Binary sampling.
    The tests generate histograms (a bit crude for now).

---
+++ tests/static/test_gaussian.c
@@ -0,0 +1,95 @@
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "sampler.h"
+#include "cpucycles.h"
+
+static sampler_t sampler;
+
+// hard-coded seed for testing
+static const uint8_t seed[SHA3_512_DIGEST_LENGTH] = {
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+};
+
+
+/*
+ * Counters for histogram:
+ * for -M < i < +M
+ * - count[M+i] = number of samples equal to i
+ * - count[2M] = number of samples >= M
+ * - count[0] = number of samples <= -M
+ */
+#define M 2048
+#define NUM_COUNTERS (2*M+1)
+static uint32_t count[NUM_COUNTERS];
+
+/*
+ * Generates ntests samples and compute statistics
+ */
+static void test_gaussian(sampler_t *sampler, uint32_t ntests) {
+  uint32_t i, failed;
+  int32_t m, p, x;
+
+  assert(ntests > 0);
+
+  for (i=0; i<NUM_COUNTERS; i++) {
+    count[i] = 0;
+  }
+  failed = 0;
+  p = 0;
+  m = 0;
+  for (i=0; i<ntests; i++) {
+    if (sampler_gauss(sampler, &x)) {
+      if (x > M) {
+	x = M; 
+      } else if (x < -M) {
+	x = -M;
+      }
+      count[M + x] ++;
+      if (x >= p) { 
+	p = x+1;
+      }
+      if (x <= m) {
+	m = x;
+      }
+    } else {
+      failed ++;
+    }
+  }
+
+  if (failed > 0) {
+    printf("The sampler failed %"PRIu32" times\n", failed);
+    ntests -= failed;
+  }
+
+  if (ntests == 0) {
+    printf("Can't generate histogram: no data\n");    
+  } else {
+    printf("Histogram\n");
+    for (x=m; x<p; x++) {
+      assert(-M <= x && x <= M);
+      printf("fraction[%"PRIi32"] = %g\n", x, ((double)count[M+x]) / ntests);
+    }
+    printf("\n");
+  }
+}
+
+int main(void){
+  printf("\nTesting Gaussian sampling\n");
+  if (! sampler_init(&sampler, 271, 22, 128, seed)) {
+    fprintf(stderr, "error initializing sampler\n");
+    return EXIT_FAILURE;
+  }
+  test_gaussian(&sampler, 20000000);
+
+  return EXIT_SUCCESS;
+}
---
+++ tests/static/test_pos_binary.c
@@ -0,0 +1,84 @@
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "sampler.h"
+#include "cpucycles.h"
+
+static sampler_t sampler;
+
+// hard-coded seed for testing
+static const uint8_t seed[SHA3_512_DIGEST_LENGTH] = {
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+  0, 1, 2, 3, 4, 5, 6, 7,
+};
+
+/*
+ * Counters for histogram
+ * - count[i] = number of samples equal to i (for i < NUM_COUNTERS)
+ * - count[NUM_COUNTER] = number of samples >= NUM_COUNTERS
+ */
+#define NUM_COUNTERS 16
+static uint32_t count[NUM_COUNTERS];
+
+/*
+ * Generates ntests samples and compute statistics
+ * - count[i] = number of samples equal to i
+ */
+static void test_pos_binary(sampler_t *sampler, uint32_t ntests) {
+  uint32_t i, x, failed, p;
+
+  assert(ntests > 0);
+
+  for (i=0; i<NUM_COUNTERS; i++) {
+    count[i] = 0;
+  }
+  failed = 0;
+  p = 0;
+  for (i=0; i<ntests; i++) {
+    if (sampler_pos_binary(sampler, &x)) {
+      if (x >= NUM_COUNTERS) {
+	x = NUM_COUNTERS - 1;
+      }
+      if (x >= p) { 
+	p = x+1;
+      }
+      count[x] ++;
+    } else {
+      failed ++;
+    }
+  }
+
+  if (failed > 0) {
+    printf("The sampler failed %"PRIu32" times\n", failed);
+    ntests -= failed;
+  }
+
+  if (ntests == 0) {
+    printf("Can't generate histogram: no data\n");    
+  } else {
+    printf("Histogram\n");
+    for (i=0; i<p; i++) {
+      printf("fraction[%"PRIu32"] = %g\n", i, ((double)count[i]) / ntests);
+    }
+    printf("\n");
+  }
+}
+
+int main(void){
+  printf("\nTesting pos binary sampling\n");
+  if (! sampler_init(&sampler, 271, 22, 128, seed)) {
+    fprintf(stderr, "error initializing sampler\n");
+    return EXIT_FAILURE;
+  }
+  test_pos_binary(&sampler, 20000000);
+
+  return EXIT_SUCCESS;
+}
