2afe1978efb881828d992014eced324b8f700cb3
Ian A. Mason <iam@csl.sri.com>
2016-11-30 11:16:31 -0800

    Toothcombing the API with BD.

--- include/bliss_b_keys.h
+++ include/bliss_b_keys.h
@@ -2,11 +2,12 @@
 #define __BLISS_B_KEYS_H__
 
 #include <stdint.h>
-#include <stdbool.h>
+#include "bliss_b_params.h"
+#include "entropy.h"
 
 /* bliss-b private key */
 typedef struct {
-  int32_t kind;                      /* index of parameter set */
+  bliss_param_t p;                   /* parameter set          */
   int32_t *f;                        /* sparse polynomial f    */
   int32_t *g;                        /* sparse polynomial g    */
   int32_t *a;                        /* NTT of f/g             */
@@ -14,56 +15,39 @@ typedef struct {
 
 /* bliss-b public key  */
 typedef struct {
-  int32_t kind;                     /* index of parameter set */
+  bliss_param_t p;                  /* parameter set          */
   int32_t *a;                       /* NTT of f/g             */
 } bliss_public_key_t;
 
 
 /* PRIVATE KEY API */
 
+/* Allocates (uninitialized space) for the private key, and generates a new private key,
+ * given a particular choice of kind. 
+ *
+ * - private_key: structure to store the result.
+ * - kind: the kind describes the choice of parameters in the particular variety of bliss-b that we are using. 
+ * - entropy: our source of randomness, an initialized entropy object.
+ * 
+ * Returns 0 on success, or a negative error code on failure.
+ */
+int32_t bliss_b_private_key_gen(bliss_private_key_t *private_key, bliss_kind_t kind, entropy_t *entropy);
+
 /* Delete the memory associated with the private_key */
 void bliss_b_private_key_delete(bliss_private_key_t *private_key);
-
-/* Allocate (uninitialized space) for the private key. 
- * Sets the kind field on the key to the given value (if valid?).
- * Returns true on success, or false on failure (and sets errno to the appropriate value)
- * (IAM2BD: do we want to return an int code instead? like the API?)
- */
-bool bliss_b_private_key_init(bliss_private_key_t *private_key, int32_t kind);
-
-
-/* Generate a new private key. The private_key should already be initialized.
- * In particular it should have a valid kind set.
- * (IAM2BD: Do we want to roll init and gen into one function)
- */
-bool bliss_b_private_key_gen(bliss_private_key_t *private_key);
 
 
 /* PUBLIC KEY API */
 
+/* Allocates (uninitialized space) for the public key, and exports it 
+ * from the given generated private_key. 
+ * Returns 0 on success, or a negative error code on failure.
+ */
+int32_t bliss_b_public_key_extract(bliss_public_key_t *public_key, const bliss_private_key_t *private_key);
+
+
 /* Delete the memory associated with the public_key */
 void bliss_b_public_key_delete(bliss_public_key_t *public_key);
-
-/* Allocate (uninitialized space) for the public key. 
- * Sets the kind field on the key to the given value (if valid?).
- * Returns true on success, or false on failure (and sets errno to the appropriate value)
- * (IAM2BD: do we want to return an int code instead? like the API?)
- */
-bool bliss_b_public_key_init(bliss_public_key_t *public_key, int32_t kind);
-
-
-/* Generate a new public key from the given generated private_key. 
- * The public_key should already be initialized.
- * In particular it should have a valid kind set, and that kind should match the
- * private key kind.
- * (IAM2BD: Do we want to roll init and gen into one function)
- */
-bool bliss_b_public_key_gen(bliss_private_key_t *private_key, bliss_public_key_t *public_key);
-
-
-
-
-
 
 #endif
 
