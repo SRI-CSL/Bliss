d456bfc7e54014fbcbd9ab66219b66dd3a56d25f
Ian A. Mason <iam@csl.sri.com>
2016-12-09 10:44:36 -0800

    Two versions of GreedySC, IV

--- src/bliss_b_signatures.c
+++ src/bliss_b_signatures.c
@@ -11,8 +11,7 @@
  */
 void greedy_sc_strongswan(const int32_t *s1, const int32_t *s2, int32_t n, const uint32_t *c_indices, int32_t kappa, int32_t *v1, int32_t *v2)
 {
-	int32_t i, k, index;
-	int32_t sign;
+  int32_t index, i, k, sign;
 
 	for (i = 0; i < n; i++)
 	{
