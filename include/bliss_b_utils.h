457b3808d3c1a215a75f16a6b632385c8b247bd9
Ian A Mason <ian.mason@sri.com>
2016-12-11 18:10:56 -0800

    Update bliss_b_utils.h

--- include/bliss_b_utils.h
+++ include/bliss_b_utils.h
@@ -1,6 +1,12 @@
 #ifndef _BLISS_B_UTILS_H
 #define _BLISS_B_UTILS_H
 
+/* 
+ *  Zeros len bytes of ptr, designed in such a way as to NOT be
+ *  optimized out by compilers.
+ *   - ptr, pointer to bytes to be zeroed.
+ *   - len, the number of bytes to be zeroed.
+ */
 extern void zero_memory(void *ptr, size_t len);
 
 #endif
