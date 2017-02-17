f003ae0c0b2fdbc1ff47875aab2f382e26b52d0f
Ian A Mason <iam@csl.sri.com>
2017-02-17 11:38:51 -0800

    Make testing a particular variant easier.

--- tests/static/test_signings.c
+++ tests/static/test_signings.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <inttypes.h>
 #include <string.h>
 
@@ -34,11 +33,8 @@ static bliss_signature_t signature;
 static int32_t COUNT = 6;
 
 int main(int argc, char* argv[]){
-
-  int32_t count;
   int32_t type;
-
-  int32_t successes = 0;
+  int32_t count;
 
   bool ok;
 
@@ -50,32 +46,22 @@ int main(int argc, char* argv[]){
 
   size_t msg_sz = strlen(text) + 1;
   
-  if(argc != 3){
-    fprintf(stderr, "Usage: %s <bliss b variant (0,1,2,3,4)> <iterations>\n", argv[0]);
-    return 1;
-  } else {
+  entropy_init(&entropy, seed);
 
-    type = atoi(argv[1]);
-    assert(BLISS_B_0 <= type && type <= BLISS_B_4);
+  for (type = BLISS_B_0; type <= BLISS_B_4; type++){
     
-    
-    COUNT = atoi(argv[2]);
-
-
-    entropy_init(&entropy, seed);
-
     for (count = 0; count < COUNT; count++){
-      
+
       ok = false;
-      
+
       fprintf(stderr, "\n\nbliss_b test type = %d, count = %d\n", type, count);
-      
+ 
       retcode = bliss_b_private_key_gen(&private_key, type, &entropy);
       if (retcode != BLISS_B_NO_ERROR){
 	fprintf(stderr, "bliss_b_private_key_gen failed: type = %d, retcode = %d\n", type, retcode);
 	goto exit;
       }
-    
+      
       retcode = bliss_b_public_key_extract(&public_key, &private_key);
       if (retcode != BLISS_B_NO_ERROR){
 	fprintf(stderr, "bliss_b_public_key_extract failed: type = %d, retcode = %d\n", type, retcode);
@@ -91,11 +77,10 @@ int main(int argc, char* argv[]){
       retcode = bliss_b_verify(&signature,  &public_key, msg, msg_sz);
       if (retcode != BLISS_B_NO_ERROR){
 	fprintf(stderr, "bliss_b_verify failed: type = %d, retcode = %d\n", type, retcode);
-	goto exit;
+      goto exit;
       }
-      
+
       ok = true;
-      successes++;
 
     exit:
       
@@ -104,13 +89,11 @@ int main(int argc, char* argv[]){
       bliss_b_public_key_delete(&public_key);
       
       bliss_signature_delete(&signature);
-      
+
       fprintf(stderr, "bliss_b type = %d:  %s\n", type, ok ? "OK" : "FAILED");
-      
+
     }
   }
-
-  fprintf(stderr, "bliss_b type = %d:  %d/%d successes\n", type, successes, COUNT);
-
+  
   return 0;
 }
